093b94d69ecd04817524201f3ea28479b6fc165e
arthuraraujo1930 <arthurenzo0223@gmail.com>
2025-05-16 09:10:50 -0300

    projeto node 2.0

--- index.js
+++ index.js
@@ -2,18 +2,42 @@ const http = require("http");
 const fs = require("fs");
 const hostname = "127.0.0.1";
 const port = 3000;
-const server = http.createServer((req, res) => {
-    console.log(req.url);
-    fs.readFile('index.html', (err, data) => {
-        res.writeHead(200, ('content-type' , 'text/html'));
-        res.write(data);
-        return res.end();
-    })
-    /*
-    res.statusCode = 200;
-    res.setHeader('content-type', 'text/plain');
-    res.end("Criando servidor Node.js");*/
+fs.writeFile('teste.txt', '2º DS PW2', (err) => {
+    if (err) {
+        console.log("Arquivo criado com sucesso")
+    }
 })
+
+fs.appendFile("teste.txt", "\niciando Back-end", (err) => {
+    if (err) throw err
+    console.log("Arquivo criado com sucesso")
+})
+
+const server = http.createServer((req, res) => {
+    if (req.url == "/login") {
+        fs.readFile('index.html', (err, data) => {
+            fs.writeFile('teste.txt', '2º DS PW2', (err) => {
+                if (err) {
+                    console.log("Arquivo criado com sucesso")
+                }
+            })
+            
+            fs.appendFile("teste.txt", "\niciando Back-end", (err) => {
+                if (err) throw err
+                console.log("Arquivo criado com sucesso")
+            })
+            
+
+            res.writeHead(200, ('content-type', 'text/html'));
+            res.write(data);
+            return res.end();
+        })
+
+    } else {
+        return req.end();
+    }
+})
+
 server.listen(port, hostname, () => {
     console.log("Servidor Rodando...");
-});
+})
