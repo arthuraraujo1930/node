1d0f70dbbfee5842a2a1b34ae8f9c9174f9c606e
arthuraraujo1930 <arthurenzo0223@gmail.com>
2025-05-23 09:04:50 -0300

    pw/23/05

--- index.js
+++ index.js
@@ -2,7 +2,30 @@ const http = require("http");
 const fs = require("fs");
 const hostname = "127.0.0.1";
 const port = 3000;
-fs.writeFile('teste.txt', '2º DS PW2', (err) => {
+const server = http.createServer((req, res)=>{
+    if (req.url == '/Sobre.html'){
+        fs.writeFile('texto.txt', '2ºDS PW2', (err) => {
+            if(err) throw err;
+            console.log('Arquivo criando')
+        })
+    }else if(req.url == '/index.html'){
+        fs.readFile('index.html', (err, data) =>{
+            res.writeHead(200, {'content-type' : 'text/html'});
+            res.write(data);
+            return res.end();
+        })
+    }else if(req.url == '/contato.html'){
+        fs.appendFile('texto.txt', '\niciando back-end', (err) =>{
+            if(err) throw err;
+            console.log('Salvo a alteração');
+        })
+    }else{
+        return res.end();
+    }
+})
+
+
+/*fs.writeFile('teste.txt', '2º DS PW2', (err) => {
     if (err) {
         console.log("Arquivo criado com sucesso")
     }
@@ -40,4 +63,4 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
     console.log("Servidor Rodando...");
-})
+})*/
